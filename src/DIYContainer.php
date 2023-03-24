bf0139c544aff24e79add7c97ca98c1b27c443be
Takashi Ogishima <t.ogishima@innovator.jp.net>
2023-03-25 08:20:54 +0900

    wip

--- src/DIYContainer.php
+++ src/DIYContainer.php
@@ -4,20 +4,18 @@ namespace PHPerKaigi2023;
 
 use Psr\Container\ContainerInterface;
 
+/**
+ * PSR-11実装の簡易自作コンテナ
+ */
 class DIYContainer implements ContainerInterface
 {
     /**
-     * 解決されたものを一時的に保存しておく配列
-     * 
-     * @var array<string,mixed>
-     */
-    private $instances = [];
-
-    /**
-     * @param array<string,Callable|class-string> $definitions DIのマッピング
+     * @param array<string,Callable|class-string> $definitions 定義（マッピング）
+     * @param array<string,mixed> $instances 解決されたものを一時的に保存しておく配列
      */
     public function __construct(
-        private array $definitions
+        private array $definitions,
+        private array $instances = []
     ){
     }
 
