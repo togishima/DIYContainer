e8384cab8cf648280da13065015d42475bb05f45
Takashi Ogishima <t.ogishima@innovator.jp.net>
2023-03-25 00:32:49 +0900

    initial

--- src/DIYContainer.php
+++ src/DIYContainer.php
@@ -2,90 +2,32 @@
 
 namespace PHPerKaigi2023;
 
-use PHPerKaigi2023\Exceptions\ContainerException;
 use Psr\Container\ContainerInterface;
-use ReflectionClass;
-use ReflectionNamedType;
 
 class DIYContainer implements ContainerInterface
 {
     /**
-     * definitions of DI mappings
-     *
-     * @var array<Callable|class-string>
-     */
-    private $definitions = [];
-
-    /**
-     * instantiated objects
-     *
+     * 解決されたものを一時的に保存しておく配列
+     * 
      * @var array<string,mixed>
      */
     private $instances = [];
 
-    public function __construct()
-    {
-        $this->definitions = include __DIR__ . '/../config/definitions.php';
+    /**
+     * @param array<string,Callable|class-string> $definitions DIのマッピング
+     */
+    public function __construct(
+        private array $definitions
+    ){
     }
 
     public function get(string $id)
     {
-        // インスタンス化されたオブジェクトがなければ作成して返す
-        if (!$this->has($id)) {
-            $this->instances[$id] = $this->make($id);
-        }
-        return $this->instances[$id];
+
     }
 
     public function has(string $id): bool
     {
-        return array_key_exists($id, $this->instances);
-    }
 
-    /**
-     * 新規にインスタンスを生成する
-     *
-     * @param string $id
-     * @return mixed
-     */
-    private function make(string $id): mixed
-    {
-        /**
-         * 一旦Callableかクラス名以外は想定しないものとする
-         */
-        if (array_key_exists($id, $this->definitions) && is_callable($this->definitions[$id])) {
-            $callable = $this->definitions[$id];
-            return $callable($this);
-        }
-        try {
-            $dependencies = $this->resolveDependencies($id);
-            return new $id(...$dependencies);
-        } catch (\Throwable $th) {
-            throw new ContainerException(
-                message: 'エラー：'.$id.'のインスタンス化に失敗しました',
-                previous: $th
-            );
-        }
-    }
-
-    /**
-     * 対象クラスのコンストラクタで定義されているクラスをインスタンス化
-     *
-     * @param string $className
-     * @return array
-     */
-    private function resolveDependencies(string $className): array
-    {
-        /** @see https://www.php.net/manual/ja/reflectionclass.getconstructor.php */
-        $constructor = (new ReflectionClass($className))->getConstructor();
-        $dependencies = [];
-        /** @see https://www.php.net/manual/ja/reflectionfunctionabstract.getparameters.php */
-        foreach($constructor->getParameters() as $param) {
-            /** @see https://www.php.net/manual/ja/reflectionparameter.gettype.php */
-            if ($param->getType() instanceof ReflectionNamedType) {
-                $dependencies[] = $this->get($param->getType()->getName());
-            }
-        }
-        return $dependencies;
     }
 }
--- tests/UnitTests/ContainerTest.php
+++ tests/UnitTests/ContainerTest.php
@@ -3,10 +3,9 @@
 namespace Test\UnitTests;
 
 use PHPerKaigi2023\DIYContainer;
+use PHPerKaigi2023\Exceptions\NotFoundException;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
-use Tests\Sample\ClassA;
-use Tests\Sample\SampleInterface;
 
 class ContainerTest extends TestCase
 {
@@ -14,41 +13,68 @@ class ContainerTest extends TestCase
 
     public function setUp(): void
     {
-        $this->container = new DIYContainer();
+        $this->container = new DIYContainer([
+            'foo' => 'bar'
+        ]);
     }
 
     /**
-     * @test
+     * @test 
      */
-    public function 具象クラス名は定義をしなくとも自動で解決できる()
+    public function 与えられた文字列に対して定義があればその通りに返す()
     {
-        // FQCNを渡せば解決してくれる
-        $sample = $this->container->get(ClassA::class);
-        $this->assertTrue($sample instanceof ClassA);
+        $bar = $this->container->get('foo');
+        $this->assertEquals($bar, 'bar');
     }
 
     /**
      * @test
      */
-    // public function 一度インスタンス化したオブジェクトはキャッシュされる()
+    // public function 与えられた文字列がクラス名だった場合オートロードできれば解決する()
     // {
-    //     // 最初はキャッシュがない状態
-    //     $this->assertFalse($this->container->has(SampleSampleInterface::class));
-    //     // 一回取り出す
-    //     $sample = $this->container->get(SampleInterface::class);
-    //     // オブジェクトが生成されたのでキャッシュされているはず...
-    //     $this->assertTrue($this->container->has(SampleInterface::class));
+    //     $sample = $this->container->get(ClassB::class);
+    //     $this->assertTrue($sample instanceof ClassB);
     // }
 
     /**
      * @test
      */
-    // public function 定義をすればインターフェースから具象クラスを解決できる()
+    // public function 定義がなくAutoloadもできなければ例外が投げられる()
+    // {
+    //     $this->expectException(NotFoundException::class);
+    //     $this->container->get('None');
+    // }
+
+    /**
+     * @test
+     */
+    // public function 一度インスタンス化したオブジェクトは解決できる()
+    // {
+    //     // AutowireのクラスはNotFoundExceptionが返る可能性がある
+    //     $this->assertFalse($this->container->has(ClassA::class));
+    //     // 一回取り出す
+    //     $sample = $this->container->get(ClassA::class);
+    //     // オブジェクトが生成されたのでtrueになる
+    //     $this->assertTrue($this->container->has(ClassA::class));
+    // }
+
+    /**
+     * @test
+     */
+    // public function インターフェースから具象クラスを解決できる()
     // {
     //     $sample = $this->container->get(SampleInterface::class);
     //     // SampleInterfaceとClassAがマッピングされる
     //     $this->assertTrue($sample instanceof ClassA);
-    //     // ClassAの内部でClassBのfuga()が呼ばれる
+    // }
+
+    /**
+     * @test
+     */
+    // public function コンストラクタで入れ子になっているクラスも解決できる()
+    // {
+    //     $sample = $this->container->get(SampleInterface::class);
+    //     // ClassAの内部でClassBのfuga()が呼ばれるはず
     //     $this->assertSame('piyo', $sample->hoge());
     // }
 }
