29b21043f096ee298f7db697d0f7be394947b6b2
Takashi Ogishima <t.ogishima@innovator.jp.net>
2023-03-25 01:13:57 +0900

    complete

--- src/DIYContainer.php
+++ src/DIYContainer.php
@@ -2,7 +2,14 @@
 
 namespace PHPerKaigi2023;
 
+use Error;
+use PHPerKaigi2023\Exceptions\ContainerException;
+use PHPerKaigi2023\Exceptions\NotFoundException;
 use Psr\Container\ContainerInterface;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionNamedType;
+use Throwable;
 
 class DIYContainer implements ContainerInterface
 {
@@ -23,11 +30,62 @@ class DIYContainer implements ContainerInterface
 
     public function get(string $id)
     {
+        // コールバックの場合は実行
+        if (array_key_exists($id, $this->definitions) && is_callable($this->definitions[$id])) {
+            $callable = $this->definitions[$id];
+            return $callable($this);
+        }
+        // クラス名なら生成を試みる
+        try {
+            return $this->make($id);
+        } catch (Error|ReflectionException $e) {
+            throw new NotFoundException(
+                message: 'Entry Not Found',
+                previous: $e
+            );
+        } catch(Throwable $th) {
+            throw new ContainerException(
+                message: 'Failed resolving ' . $id,
+                previous: $th
+            );
+        }
+    }
 
+    private function make(string $id)
+    {
+        if (!array_key_exists($id, $this->instances)) {
+            $dependencies = $this->resolveDependencies($id);
+            $this->instances[$id] = new $id(...$dependencies);
+        }
+        return $this->instances[$id];
+    }
+
+    /**
+     * 
+     *
+     * @param class-string $id
+     * @return array<mixed>
+     */
+    private function resolveDependencies(string $id)
+    {
+        /** @see https://www.php.net/manual/ja/reflectionclass.getconstructor.php */
+        $constructor = (new ReflectionClass($id))->getConstructor();
+        $dependencies = [];
+        /** @see https://www.php.net/manual/ja/reflectionfunctionabstract.getparameters.php */
+        foreach ($constructor->getParameters() as $param) {
+            /** @see https://www.php.net/manual/ja/reflectionparameter.gettype.php */
+            if ($param->getType() instanceof ReflectionNamedType) {
+                $dependencies[] = $this->get($param->getType()->getName());
+            }
+        }
+        return $dependencies;
     }
 
     public function has(string $id): bool
     {
-
+        if (array_key_exists($id, $this->definitions) || array_key_exists($id, $this->instances)) {
+            return true;
+        }
+        return false;
     }
 }
--- tests/UnitTests/ContainerTest.php
+++ tests/UnitTests/ContainerTest.php
@@ -3,8 +3,12 @@
 namespace Test\UnitTests;
 
 use PHPerKaigi2023\DIYContainer;
+use PHPerKaigi2023\Exceptions\NotFoundException;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
+use Tests\Sample\ClassA;
+use Tests\Sample\ClassB;
+use Tests\Sample\SampleInterface;
 
 class ContainerTest extends TestCase
 {
@@ -14,6 +18,7 @@ class ContainerTest extends TestCase
     {
         $this->container = new DIYContainer([
             'foo' => fn() => 'bar',
+            SampleInterface::class => fn(ContainerInterface $c) => $c->get(ClassA::class)
         ]);
     }
 
@@ -29,51 +34,50 @@ class ContainerTest extends TestCase
     /**
      * @test
      */
-    // public function 与えられた文字列がクラス名だった場合オートロードできれば解決する()
-    // {
-    //     $sample = $this->container->get(ClassB::class);
-    //     $this->assertTrue($sample instanceof ClassB);
-    // }
+    public function 与えられた文字列がクラス名だった場合オートロードできれば解決する()
+    {
+        $sample = $this->container->get(ClassB::class);
+        $this->assertTrue($sample instanceof ClassB);
+    }
 
     /**
      * @test
      */
-    // public function 定義がなくAutoloadもできなければ例外が投げられる()
-    // {
-    //     $this->expectException(NotFoundException::class);
-    //     $this->container->get('None');
-    // }
+    public function 定義がなくAutoloadもできなければ例外が投げられる()
+    {
+        $this->expectException(NotFoundException::class);
+        $this->container->get('None');
+    }
 
     /**
      * @test
      */
-    // public function 一度インスタンス化したオブジェクトは解決できる()
-    // {
-    //     // AutowireのクラスはNotFoundExceptionが返る可能性がある
-    //     $this->assertFalse($this->container->has(ClassA::class));
-    //     // 一回取り出す
-    //     $sample = $this->container->get(ClassA::class);
-    //     // オブジェクトが生成されたのでtrueになる
-    //     $this->assertTrue($this->container->has(ClassA::class));
-    // }
+    public function 一度インスタンス化したオブジェクトは解決できる()
+    {
+        // AutowireのクラスはNotFoundExceptionが返る可能性がある
+        $this->assertFalse($this->container->has(ClassB::class));
+        // 一回取り出す
+        $sample = $this->container->get(ClassB::class);
+        // オブジェクトが生成されたのでtrueになる
+        $this->assertTrue($this->container->has(ClassB::class));
+    }
 
     /**
      * @test
      */
-    // public function インターフェースから具象クラスを解決できる()
-    // {
-    //     $sample = $this->container->get(SampleInterface::class);
-    //     // SampleInterfaceとClassAがマッピングされる
-    //     $this->assertTrue($sample instanceof ClassA);
-    // }
+    public function 入れ子になっているクラスを解決できる()
+    {
+        $sample = $this->container->get(ClassA::class);
+        // ClassAの内部でClassBのfuga()が呼ばれるはず
+        $this->assertSame('piyo', $sample->hoge());
+    }
 
     /**
      * @test
      */
-    // public function コンストラクタで入れ子になっているクラスも解決できる()
-    // {
-    //     $sample = $this->container->get(SampleInterface::class);
-    //     // ClassAの内部でClassBのfuga()が呼ばれるはず
-    //     $this->assertSame('piyo', $sample->hoge());
-    // }
+    public function インターフェースとのマッピングができる()
+    {
+        $sample = $this->container->get(SampleInterface::class);
+        $this->assertTrue($sample instanceof ClassA);
+    }
 }
